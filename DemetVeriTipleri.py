06c69387cea896448945e8db072b09b51fe2be90
fatihkaratekin <fatihkaratekin17@hotmail.com>
2020-12-03 12:16:24 +0300

    demetler

---
+++ DemetVeriTipleri.py
@@ -0,0 +1,16 @@
+
+
+#Demetelerin içindenki verileri degiştiremezsin ve demetler(tuple diyede geçer ) () diye gösterilir.
+
+demet = (1,2,3,4,5,5,6,6,7,8,9,10)
+
+print(demet)
+
+#.count fonksiyonu ile demetin içindeki bir verinin kaç kere geçtigini bulabiliriz.
+
+print(demet.count(5) , (demet.count(10)))
+
+# .index fonksiyonu ile demetin elemanın kaçıncı indekste oldugunu öğrenebiliriz.
+
+print(demet.index(4) , demet.index(1))
+
